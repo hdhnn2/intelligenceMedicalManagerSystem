29660164fd32b4712dac9a11331298a8a364fd07
hdhnn2 <1961834262@qq.com>
2020-06-20 09:25:18 +0800

    init

---
+++ src/main/java/com/hd/imms/IntelligenceMedicalManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hd.imms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligenceMedicalManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligenceMedicalManagerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hd/imms/IntelligenceMedicalManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hd.imms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligenceMedicalManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
