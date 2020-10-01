3c0c2e91e7bccec65d53aacb702d9873610feb51
hdhnn2 <Git@2019!>
2020-10-01 23:09:52 +0800

    log4j2 change to logback

--- src/main/java/com/hd/imms/IntelligenceMedicalManagerSystemApplication.java
+++ src/main/java/com/hd/imms/IntelligenceMedicalManagerSystemApplication.java
@@ -1,10 +1,12 @@
 package com.hd.imms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@Slf4j
 public class IntelligenceMedicalManagerSystemApplication {
 
     public static void main(String[] args) {
