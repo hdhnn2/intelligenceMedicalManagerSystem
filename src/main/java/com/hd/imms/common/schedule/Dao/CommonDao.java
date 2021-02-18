14f4c5337d187d8e9b3c7bb518e4b76c507c947f
hdhnn2 <Git@2019!>
2021-02-18 17:01:12 +0800

    核酸检测接口

--- src/main/java/com/hd/imms/common/schedule/Dao/CommonDao.java
+++ src/main/java/com/hd/imms/common/schedule/Dao/CommonDao.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 /**
  * 功能：通用数据库查询
- * 日期：2021-02-07
+ * 日期：2021-02-18
  * 作者：hnn
  */
 @Repository
@@ -18,12 +18,22 @@ public class CommonDao {
     @Autowired
     JdbcTemplate ds1JdbcTemplate;
     /**
-     * 核酸检测信息
+     * 查询核酸检测未上传结果
      * @return
      */
     public List<Map<String, Object>> cxhsjcxx(){
-        String sql = "select t.dept_name from dept_dict t where dept_code='010101' ";
+        String sql = "select * from v_HNN_HSJC_SC t ";
         List<Map<String, Object>> list = ds1JdbcTemplate.queryForList(sql);
         return list;
+    }
+    /**
+     *  功能: 更新核酸检测上传信息
+     *  date: 2021-02-18
+     */
+    public void updateUploadInfo(String sqbh){
+        String sql = "update HNN_HSJC_SC t set t.SCCS=nvl(t.sccs,0)+1,scjg=1,scsj=sysdate where t.remark=?";
+        Object[] args = new Object[1];
+        args[0] = sqbh;
+        ds1JdbcTemplate.update(sql, args);
     }
 }
--- src/main/java/com/hd/imms/common/schedule/service/CommonService.java
+++ src/main/java/com/hd/imms/common/schedule/service/CommonService.java
@@ -1,5 +1,7 @@
 package com.hd.imms.common.schedule.service;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.hd.imms.common.schedule.Dao.CommonDao;
 import com.hd.imms.common.schedule.Dao.LisDao;
 import com.hd.imms.common.utils.EncryptionUtils;
@@ -11,8 +13,10 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Service
 public class CommonService {
@@ -26,32 +30,11 @@ public class CommonService {
      */
     public void hsjcxxsc() throws Exception {
         String key = "1234";
+        //查询当天已上传成功单号
         List<Map<String, Object>> list = commonDao.cxhsjcxx();
         if(list != null){
             for(Map<String, Object> map : list){
-                for(Map.Entry<String, Object> entry : map.entrySet()){
-                    String content = entry.getValue().toString();
-                    System.err.println("key: " + entry.getKey()+",  value: "+entry.getValue());
-                    System.err.println("before encrypt  content: "+content);
-                    // 加密数据, 返回密文
-                    byte[] cipherBytes = EncryptionUtils.encrypt(content.getBytes(), key.getBytes());
-                    System.out.println("encrypt  content"+new String(cipherBytes));
-                    // 解密数据, 返回明文
-                    byte[] plainBytes = EncryptionUtils.decrypt(cipherBytes, key.getBytes());
-                    // 输出解密后的明文
-                    System.out.println("decryption"+new String(plainBytes));
-                }
-
-            }
-        }
-        List<Map<String, Object>> list1 = lisDao.cxhsjcxx();
-        if(list1 != null){
-            for(Map<String, Object> map : list1){
-                for(Map.Entry<String, Object> entry : map.entrySet()){
-                    String content = entry.getValue().toString();
-                    System.err.println("lis map key: " + entry.getKey()+",  value: "+entry.getValue());;
-                }
-
+                JSONObject json = this.convMapToJSON(map);
             }
         }
     }
@@ -90,5 +73,54 @@ public class CommonService {
             }
         }
         return result;
+    }
+    /**
+     * 把核酸上传结果信息组装成
+     */
+    private JSONObject convMapToJSON(Map<String, Object> map){
+        JSONObject retJSON = new JSONObject();
+        retJSON.put("posId", (String)map.get("posId"));
+        retJSON.put("detectOrgId", (String)map.get("detectOrgId"));
+        retJSON.put("detectOrgName", (String)map.get("detectOrgName"));
+        retJSON.put("detectType", (String)map.get("detectType"));
+        retJSON.put("certificateType", (String)map.get("certificateType"));
+        retJSON.put("idNo", (String)map.get("idNo"));
+        retJSON.put("name", (String)map.get("name"));
+        retJSON.put("gender", (String)map.get("gender"));
+        retJSON.put("birthday", (String)map.get("birthday"));
+        retJSON.put("employer", (String)map.get("employer"));
+        retJSON.put("telPhone", (String)map.get("telPhone"));
+        retJSON.put("ayCloseContact", (String)map.get("ayCloseContact"));
+        retJSON.put("address", (String)map.get("address"));
+        retJSON.put("detectPersonName", (String)map.get("detectPersonName"));
+        retJSON.put("detectTime", (String)map.get("detectTime"));
+        retJSON.put("samplingTime", (String)map.get("samplingTime"));
+        retJSON.put("samplingPersonName", (String)map.get("samplingPersonName"));
+        retJSON.put("reportPersonName", (String)map.get("reportPersonName"));
+        retJSON.put("reportTime", (String)map.get("reportTime"));
+        retJSON.put("auditPersonName", (String)map.get("auditPersonName"));
+        retJSON.put("auditTime", (String)map.get("auditTime"));
+        retJSON.put("specimenNumber", (String)map.get("specimenNumber"));
+        retJSON.put("remark", (String)map.get("remark"));
+        //结果信息
+        JSONArray arr = new JSONArray();
+        JSONObject resultJSON = new JSONObject();
+        resultJSON.put("itemCode", (String)map.get("itemCode"));
+        resultJSON.put("itemName", (String)map.get("itemName"));
+        resultJSON.put("result", (String)map.get("result"));
+        resultJSON.put("unit", (String)map.get("unit"));
+        resultJSON.put("limit", (String)map.get("limit"));
+        arr.add(resultJSON);
+        retJSON.put("resultList", arr);
+        /*System.err.println("key: " + entry.getKey()+",  value: "+entry.getValue());
+        System.err.println("before encrypt  content: "+content);
+        // 加密数据, 返回密文
+        byte[] cipherBytes = EncryptionUtils.encrypt(content.getBytes(), key.getBytes());
+        System.out.println("encrypt  content"+new String(cipherBytes));
+        // 解密数据, 返回明文
+        byte[] plainBytes = EncryptionUtils.decrypt(cipherBytes, key.getBytes());
+        // 输出解密后的明文
+        System.out.println("decryption"+new String(plainBytes));*/
+        return retJSON;
     }
 }
