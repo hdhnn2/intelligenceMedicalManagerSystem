684fb96b3aea8af938d1c9921879df32b3c65d4d
hdhnn2 <Git@2019!>
2021-05-24 09:39:06 +0800

    logout

--- src/main/java/com/hd/imms/common/config/WebSecurityConfig.java
+++ src/main/java/com/hd/imms/common/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.hd.imms.common.config;
 
+import com.alibaba.fastjson.JSONObject;
 import com.hd.imms.common.filter.JwtAuthenticationFilter;
 import com.hd.imms.common.filter.JwtLoginFilter;
 import com.hd.imms.common.security.JwtAuthenticationProvider;
@@ -15,9 +16,17 @@ import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  *  功能：权限认证
@@ -55,11 +64,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/info").permitAll()
                 // 分诊URL
                 .antMatchers("/auth/**").permitAll()
-                .antMatchers("/performance/**").permitAll()
+                //.antMatchers("/performance/**").permitAll()
                 // 其他所有请求需要身份认证
                 .anyRequest().authenticated();
         // 退出登录处理器
-        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+        http.logout()
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login")
+            .logoutSuccessHandler(new LogoutSuccessHandler() {
+                @Override
+                public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                    JSONObject retJSON = new JSONObject();
+                    retJSON.put("code", 200);
+                    retJSON.put("data", "");
+                    httpServletResponse.getWriter().write(retJSON.toJSONString());
+                }
+            })
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+                    }
+                })
+             .invalidateHttpSession(true)
+             .deleteCookies("token");
         // 开启登录认证流程过滤器
         //http.addFilterBefore(new JwtLoginFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class);
         // 访问控制时登录状态检查过滤器
